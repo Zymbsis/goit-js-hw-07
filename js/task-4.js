ca0cc64c2793475c03b7bf228148f768c7809861
Zymbsis <Aleksandra.aleksashka1809@gmail.com>
2023-12-28 23:21:11 +0100

    task4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,21 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const email = event.target.elements.email.value;
+  const password = event.target.elements.password.value;
+
+  if (email === '' || password === '') {
+    return alert('All form fields must be filled in');
+  }
+  const formData = {
+    Email: `${email}`,
+    Password: `${password}`,
+  };
+  console.log(formData);
+
+  event.target.reset();
+}
