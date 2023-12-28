a71391001eb41be5cc5a435038a1a5917fd3aeda
Zymbsis <Aleksandra.aleksashka1809@gmail.com>
2023-12-28 23:50:14 +0100

    task4

--- js/task-4.js
+++ js/task-4.js
@@ -5,15 +5,15 @@ form.addEventListener('submit', handleSubmit);
 function handleSubmit(event) {
   event.preventDefault();
 
-  const email = event.target.elements.email.value;
-  const password = event.target.elements.password.value;
+  const email = event.target.elements.email.value.trim();
+  const password = event.target.elements.password.value.trim();
 
   if (email === '' || password === '') {
     return alert('All form fields must be filled in');
   }
   const formData = {
-    Email: email.trim(),
-    Password: password.trim(),
+    Email: email,
+    Password: password,
   };
   event.target.reset();
   console.log(formData);
