e1d0f79002eb957db299652484055f225c818b3e
Zymbsis <Aleksandra.aleksashka1809@gmail.com>
2023-12-28 16:52:14 +0100

    task3

--- js/task-3.js
+++ js/task-3.js
@@ -2,6 +2,12 @@ const textInput = document.querySelector('#name-input');
 const output = document.querySelector('#name-output');
 
 textInput.addEventListener('input', (event) => {
-  const userValue = event.currentTarget.value.trim();
+  const userValue = event.currentTarget.value
+    .trim()
+    .split('')
+    .map((item, index, arr) =>
+      index === 0 || arr[index - 1] === ' ' ? item.toUpperCase() : item,
+    )
+    .join('');
   output.textContent = userValue ? userValue : 'Anonymous';
 });
