60f37b745d6888baeb4786bc313895d39a132316
Zymbsis <Aleksandra.aleksashka1809@gmail.com>
2023-12-29 11:06:35 +0100

    task5

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,13 @@
+const button = document.querySelector('.change-color');
+const changeColor = document.querySelector('.color');
+
+button.addEventListener('click', getRandomHexColor);
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
+  const newColor = `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+
+  changeColor.textContent = newColor;
+  document.body.style.backgroundColor = newColor;
 }
